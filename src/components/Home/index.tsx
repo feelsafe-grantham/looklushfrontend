29bd883a6e10944857f2eb26da5d3d617b3c4166
Aman Pandey <skytechno00@gmail.com>
2025-05-05 17:44:02 +0530

    changes done

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -125,7 +125,24 @@ const Home = () => {
         { label: "Home", value: "/", },
         { label: "Home", value: "/", },
     ]
+    // faq
 
+    const faq = [
+        { text: "Acne Star", image: "/images/problem1.png" },
+        { text: "Hydrafacial", image: "/images/problem2.png" },
+        { text: "Laser Hair Removal", image: "/images/problem3.png" },
+        { text: "Hifu Facial", image: "/images/problem4.png" },
+        { text: "Lip Lightening", image: "/images/problem5.png" },
+    ]
+    //faqs
+
+    const qna = [
+        { question: "I am best coder in the world", answer: "   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat itaque cupiditate iure adipisci soluta pariatur, consequuntur placeat quisquam deleniti necessitatibus?" },
+        { question: "I am best coder in the world", answer: "   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat itaque cupiditate iure adipisci soluta pariatur, consequuntur placeat quisquam deleniti necessitatibus?" },
+        { question: "I am best coder in the world", answer: "   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat itaque cupiditate iure adipisci soluta pariatur, consequuntur placeat quisquam deleniti necessitatibus?" },
+        { question: "I am best coder in the world", answer: "   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat itaque cupiditate iure adipisci soluta pariatur, consequuntur placeat quisquam deleniti necessitatibus?" },
+        { question: "I am best coder in the world", answer: "   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat itaque cupiditate iure adipisci soluta pariatur, consequuntur placeat quisquam deleniti necessitatibus?" },
+    ]
 
     return (
         <div className={`${styles.homeContainer}`}>
@@ -312,7 +329,7 @@ const Home = () => {
                 </table>
             </div>
             {/* price table ends */}
-            {/* faq section / Form section  */}
+            {/* service section / Form section  */}
             <div className={`${styles.sectionHeadingContainer}`}>
                 <h3 className={`${styles.sectionHeading}`}>
                     Welcome to Looklush Asthetic <br />
@@ -350,7 +367,46 @@ const Home = () => {
                     </form>
                 </div>
             </div>
+            {/* service section / Form section  ends */}
+            {/* Faq section starts */}
+            <div className={`${styles.sectionHeadingContainer}`}>
+                <h3 className={`${styles.sectionHeading}`}>
+                    Frequenntly Asked <br />
+                    Questions
+                </h3>
+                <img
+                    alt="pink decorative sign"
+                    src="/images/pink-sign.png"
+                    className={`${styles.sectionHeadingImage}`}
+                />
+            </div>
+            <div className={`${styles.faqContainer}`}>
+                {faq.map((faq, index) =>
+                    <div key={index} className={styles.iconWrapper}>
+                        <div className={styles.imageContainer}>
+                            <img src={faq.image} className={styles.faqImage} />
+                            <img src="/images/faqborder.png" alt={faq.text} className={styles.border} />
+                        </div>
+                        <p className={styles.label}>{faq.text}</p>
+                    </div>
+                )}
 
+            </div>
+            {/* faqs */}
+            <div className={`${styles.qnaContainer}`}>
+                <ul className={`${styles.questionsContainer}`} >
+                    {qna.map((item, index) => (
+                        <li key={index} className={styles.card}>
+                            <span className={styles.number}> {(index + 1).toString().padStart(2, '0')}</span>
+                            <p className={styles.text}>{item.question}</p>
+                        </li>
+                    ))}
+                </ul>
+                <div className={`${styles.answerContainer}`}>
+                    <p className={styles.answer}>{qna[0].answer}</p>
+                    <img className={`${styles.qnaImage}`} src="/images/qnaImage.gif" alt="qna image" />
+                </div>
+            </div>
             <div className="h-96 w-full border-2 border-red-900"></div>
         </div>
     );
