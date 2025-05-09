fc09d03d39cfeff1cf0a370b7ec16763667f469f
Aman Pandey <skytechno00@gmail.com>
2025-05-09 17:23:54 +0530

    done

--- src/components/Treatments/index.tsx
+++ src/components/Treatments/index.tsx
@@ -1,5 +1,130 @@
+import SectionHeading from "../common/SectionHeading";
+import SectionHeadPara from "../common/SectionHeadPara";
 import styles from "./Treatments.module.css"
 const Treatments = () => {
+    const images = [
+        "/images/after1.png",
+        "/images/after2.png",
+        "/images/after3.png",
+    ]
+    const benefits = [
+        {
+            title: "Effective for All Skin Types",
+            description: "Works on light and dark skin tones, even tanned skin.",
+        },
+        {
+            title: "Fast Sessions",
+            description: "Covers large areas quickly, reducing treatment time.",
+        },
+        {
+            title: "More Comfortable",
+            description: "Advanced cooling technology enhances comfort during treatment.",
+        },
+        {
+            title: "Long-Lasting Results",
+            description: "Leads to desired hair reduction with multiple sessions.",
+        },
+        {
+            title: "Works on All Hair Types",
+            description: "Effective for both fine and coarse hair.",
+        },
+        {
+            title: "Fewer Sessions Needed",
+            description: "More powerful than many other lasers, requiring fewer treatments.",
+        },
+        {
+            title: "Safe and FDA-Approved",
+            description: "A proven and medically safe procedure.",
+        },
+        {
+            title: "Minimal Downtime",
+            description: "Patients can resume daily activities immediately.",
+        },
+    ];
+    const treatmentCards = [
+        {
+            title: "Laser Hair Removal",
+            description:
+                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+            rating: 4.8,
+            customers: "300+",
+            image1: "/images/after1.png", // replace with actual image path
+            image2: "/images/after1.png", // replace with actual image path
+        },
+        {
+            title: "Laser Hair Removal",
+            description:
+                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+            rating: 4.8,
+            customers: "300+",
+            image1: "/images/after1.png", // replace with actual image path
+            image2: "/images/after1.png", // replace with actual image path
+        },
+        {
+            title: "Laser Hair Removal",
+            description:
+                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+            rating: 4.8,
+            customers: "300+",
+            image1: "/images/after1.png", // replace with actual image path
+            image2: "/images/after1.png", // replace with actual image path
+        },
+        {
+            title: "Laser Hair Removal",
+            description:
+                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+            rating: 4.8,
+            customers: "300+",
+            image1: "/images/after1.png", // replace with actual image path
+            image2: "/images/after1.png", // replace with actual image path
+        },
+        {
+            title: "Laser Hair Removal",
+            description:
+                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+            rating: 4.8,
+            customers: "300+",
+            image1: "/images/after1.png", // replace with actual image path
+            image2: "/images/after1.png", // replace with actual image path
+        },
+        {
+            title: "Laser Hair Removal",
+            description:
+                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+            rating: 4.8,
+            customers: "300+",
+            image1: "/images/after1.png", // replace with actual image path
+            image2: "/images/after1.png", // replace with actual image path
+        },
+        {
+            title: "Laser Hair Removal",
+            description:
+                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+            rating: 4.8,
+            customers: "300+",
+            image1: "/images/after1.png", // replace with actual image path
+            image2: "/images/after1.png", // replace with actual image path
+        },
+        {
+            title: "Laser Hair Removal",
+            description:
+                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+            rating: 4.8,
+            customers: "300+",
+            image1: "/images/after1.png", // replace with actual image path
+            image2: "/images/after1.png", // replace with actual image path
+        },
+        {
+            title: "Laser Hair Removal",
+            description:
+                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+            rating: 4.8,
+            customers: "300+",
+            image1: "/images/after1.png", // replace with actual image path
+            image2: "/images/after1.png", // replace with actual image path
+        },
+
+    ]
     return (
         <div className={`${styles.treatmentContainer}`}>
             <div className={`${styles.treatmentHeroSection}`}>
@@ -28,7 +153,7 @@ const Treatments = () => {
                         </div>
                     </div>
                     <div className={`${styles.buttonContainer}`}>
-                        <button className={`${styles.button}`}>Book Now</button>
+                        <button className={`${styles.heroButton}`}>Book Now</button>
                     </div>
                 </div>
                 <div className={`${styles.heroImageContainer}`}>
@@ -61,6 +186,52 @@ const Treatments = () => {
                         <button type="submit" className={styles.bookButton}>Book Now!</button>
                     </div>
                 </form>
+
+            </div>
+            <SectionHeading line1="Benifits of Premelase hair" line2="removal procedure" />
+            <SectionHeadPara para="Looklush clinic treats each patient with empathy and discretion. A patient arriving at Looklush Clinic is educated about their condition prior to treatment." />
+            <div className={styles.patientImageContainer}>
+                {images.map((img, idx) => <img key={idx} className={styles.patientImage} src={img} alt="patientImage" />)}
+            </div>
+            <SectionHeading line1="Here are the key points about Primelase" line2="laser hair removal " />
+            <div className={styles.gridContainer}>
+                {benefits.map((item, index) => (
+                    <div key={index} className={styles.card}>
+                        <strong>{item.title}</strong> – {item.description}
+                    </div>
+                ))}
+            </div>
+            <div className={`${styles.treatmentCardContainer}`}>
+                {treatmentCards.map((treatmentCard, index) =>
+                    <div key={index} className={styles.treatmentCard}>
+                        <div className={styles.imageContainer}>
+                            <img
+                                src={treatmentCard.image1}
+                                alt={treatmentCard.title}
+                                className={styles.cardImage}
+                            />
+                            <img
+                                src={treatmentCard.image2}
+                                alt={treatmentCard.title}
+                                className={styles.cardImage}
+                            />
+                        </div>
+                        <h3 className={styles.title}>{treatmentCard.title}</h3>
+                        <p className={styles.description}>{treatmentCard.description}</p>
+
+                        <div className={styles.stats}>
+                            <div className={styles.stat}>
+                                <span className={styles.rating}>{treatmentCard.rating}</span>
+                                <span className={styles.stars}>⭐⭐⭐⭐⭐</span>
+                            </div>
+                            <div className={styles.stat}>
+                                <span className={styles.customers}>{treatmentCard.customers}</span>
+                                <span className={styles.label}>Satisfied Customers</span>
+                            </div>
+                        </div>
+
+                        <button className={styles.button}>Book Now!</button>
+                    </div>)}
 
             </div>
         </div >
