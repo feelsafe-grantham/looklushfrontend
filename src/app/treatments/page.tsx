55aa34c8e4492beb739e73eca4742a4593e41f59
Aman Pandey <skytechno00@gmail.com>
2025-05-09 16:15:51 +0530

    treatment hero section

--- src/app/treatments/page.tsx
+++ src/app/treatments/page.tsx
@@ -1,10 +1,6 @@
+import TreatmentsComp from "@/components/Treatments";
 export default function Treatments() {
     return (
-        <div>
-            <h1>Treatments</h1>
-            <p>We offer a variety of treatments to help you look and feel your best.</p>
-            <p>From skincare to body treatments, we have something for everyone.</p>
-            <p>Contact us to learn more about our services!</p>
-        </div>
+        <TreatmentsComp />
     );
 }
--- src/components/Treatments/index.tsx
+++ src/components/Treatments/index.tsx
@@ -1,8 +1,42 @@
+import styles from "./Treatments.module.css"
 const Treatments = () => {
     return (
-        <div>
-            asdf
-        </div>
+        <div className={`${styles.treatmentContainer}`}>
+            <div className={`${styles.treatmentHeroSection}`}>
+                <img className={`${styles.treatmentLeafRight}`} src="/images/treatment-leaf.png" alt="treatment-leaf" />
+                <div className={`${styles.treatmentHeroLeft}`}>
+                    <div className={`${styles.heroHeadingContainer}`}>
+                        <h2 className={`${styles.heroHeading}`}>
+                            Primelase Laser Hair <br />
+                            removal treatment
+                            <img className={`${styles.pinkSign}`} src="/images/pink-sign.png" alt="pink-sign" />
+                        </h2>
+                    </div>
+                    <div className={`${styles.dataContainer}`}>
+                        <div className={`${styles.ratingContainer}`}>
+                            <span className={`${styles.rating}`}>
+                                4.9
+                            </span>
+                            <span className={`${styles.stars}`}>
+                                ⭐⭐⭐⭐⭐
+                            </span>
+                        </div>
+                        <span className={`${styles.line}`}></span>
+                        <div className={`${styles.clientsContainer}`}>
+                            <span className={`${styles.clients}`}>300+</span>
+                            <span className={`${styles.clientsText}`}>Satisfied Customers</span>
+                        </div>
+                    </div>
+                    <div className={`${styles.buttonContainer}`}>
+                        <button className={`${styles.button}`}>Book Now</button>
+                    </div>
+                </div>
+                <div className={`${styles.heroImageContainer}`}>
+                    <img className={`${styles.heroImage}`} src="/images/treatmentHero.png" alt="heroImage" />
+                </div>
+                <img className={`${styles.treatmentLeafLeft}`} src="/images/treatment-leaf-left.png" alt="treatment-leaf" />
+            </div>
+        </div >
 
     )
 }
