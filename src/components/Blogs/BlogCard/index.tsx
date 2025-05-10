664404053c84b1340a5cd2a8ce170f4beba8f0ac
Aman kumar Pandey <137035478+iamanpandey11@users.noreply.github.com>
2025-05-10 17:30:10 +0530

    changes done

--- src/components/Blogs/BlogCard/index.tsx
+++ src/components/Blogs/BlogCard/index.tsx
@@ -1,35 +1,42 @@
-import styles from "./BlogCard.module.css"
+import Link from "next/link";
+import styles from "./BlogCard.module.css";
 const BlogCard = () => {
-    return (
-        <div className={styles.card}>
-            <div className={styles.textSection}>
-                <h2 className={styles.heading}>
-                    Welcome to our Aesthetic Clinic We<br />
-                    use most recent and up to date<br />
-                    Aesthetic and Laser Technology
-                </h2>
-                <p className={styles.meta}>Author Name | 26 June</p>
-                <p className={styles.description}>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                    been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-                    a galley of type and scrambled it to make a type specimen book. It has survived not only
-                    five centuries, but also the leap into electronic typesetting, remaining essentially
-                    unchanged.
-                </p>
-                <p className={styles.description}>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                    been the industry's standard dummy text ever since the 1500s, when an unknown printer.
-                </p>
-                <button className={styles.button}>Show More!</button>
-            </div>
-            <div className={styles.imageSection}>
-                <img
-                    src="/images/after1.png"
-                    alt="Aesthetic Treatment"
-                    className={styles.image}
-                />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.card}>
+      <div className={styles.textSection}>
+        <h2 className={styles.heading}>
+          Welcome to our Aesthetic Clinic We
+          <br />
+          use most recent and up to date
+          <br />
+          Aesthetic and Laser Technology
+        </h2>
+        <p className={styles.meta}>Author Name | 26 June</p>
+        <p className={styles.description}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged.
+        </p>
+        <p className={styles.description}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer.
+        </p>
+        <Link href="#" className={styles.button}>
+          Show More!
+        </Link>
+      </div>
+      <div className={styles.imageSection}>
+        <img
+          src="/images/after1.png"
+          alt="Aesthetic Treatment"
+          className={styles.image}
+        />
+      </div>
+    </div>
+  );
+};
 export default BlogCard;
--- src/components/Blogs/LaserBurn/index.tsx
+++ src/components/Blogs/LaserBurn/index.tsx
@@ -2,27 +2,44 @@ import React from "react";
 import styles from "./LaserBurn.module.css";
 
 const LaserBurnSection = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.imageWrapper}>
-                <button className={styles.navArrow}>❮</button>
-                <img
-                    src="/images/homecard1.png"
-                    alt="Laser Burn Treatment"
-                    className={styles.image}
-                />
-                <button className={styles.navArrow}>❯</button>
-            </div>
-            <h2 className={styles.title}>Is Laser Burns?</h2>
-            <div className={styles.buttonGroup}>
-                <button className={styles.infoButton}>What is a Laser Burn?</button>
-                <button className={styles.infoButton}>How it works?</button>
-                <button className={styles.infoButton}>Total Sitting?</button>
-                <button className={styles.infoButton}>Is it painful?</button>
-            </div>
-            <button className={styles.checkoutButton}>Book Now</button>
-        </div>
-    );
+  const btnLinks = [
+    {
+      label: "What is a Laser Burn?",
+      url: "",
+    },
+    {
+      label: "How it works?",
+      url: "",
+    },
+    {
+      label: "Total Sitting?",
+      url: "",
+    },
+    {
+      label: "Is it painful?",
+      url: "",
+    },
+  ];
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageWrapper}>
+        <button className={styles.navArrow}>❮</button>
+        <img
+          src="/images/homecard1.png"
+          alt="Laser Burn Treatment"
+          className={styles.image}
+        />
+        <button className={styles.navArrow}>❯</button>
+      </div>
+      <h2 className={styles.title}>Is Laser Burns?</h2>
+      <div className={styles.buttonGroup}>
+        {btnLinks.map((btn) => (
+          <button className={styles.infoButton}>{btn.label}</button>
+        ))}
+      </div>
+      <button className={styles.checkoutButton}>Book Now</button>
+    </div>
+  );
 };
 
 export default LaserBurnSection;
