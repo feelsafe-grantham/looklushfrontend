ec386a4bb74e2db1fdff5759a97c7ca32250fd29
Aman kumar Pandey <137035478+iamanpandey11@users.noreply.github.com>
2025-05-22 12:38:13 +0530

    some more done

--- src/components/TreatmentsDetail/ResultsSection/index.tsx
+++ src/components/TreatmentsDetail/ResultsSection/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useEffect, useRef } from "react";
 import styles from "./ResultsSection.module.css";
 import SectionHeading from "@/components/common/SectionHeading";
@@ -6,60 +6,62 @@ import useResult from "./useResults";
 import VideoShimmer from "@/components/common/Loading/VideoShimmer";
 
 const ResultsSection = ({ id }: { id: number | undefined }) => {
-    const { result, loading } = useResult(id);
-    const videoRef = useRef<HTMLVideoElement | null>(null);
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            if (videoRef.current && result?.video_url) {
-                videoRef.current.src = result.video_url;
-            }
-        }, 300);
+  const { result, loading } = useResult(id);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (videoRef.current && result?.video_url) {
+        console.log("This is url: ", result.video_url);
+        videoRef.current.src = result.video_url;
+      } else {
+        console.log("This is not url: ", result?.video_url);
+      }
+    }, 300);
 
-        return () => {
-            clearTimeout(timeout);
-        };
-    }, []);
-    if (loading) return <VideoShimmer />
-    return (
-        <div className={styles.container}>
-            <div className={styles.textSection}>
-                {result?.title1 && <SectionHeading line1={result?.title1} line2={result?.title2} />}
-                <p className={styles.description}>
-                    {result?.description}
-                </p>
-                <div className={styles.imageCarousel}>
-                    <div className={`${styles.imagesContainer}`}>
-                        {result?.image.map((image: any, index: number) =>
-                            <img key={index} src={image.image} alt="Male patient" />)
-                        }
-
-                    </div>
-                    <div className={styles.dots}>
-                        <span className={styles.dot}></span>
-                        <span className={styles.dot}></span>
-                        <span className={styles.dot}></span>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.videoSection}>
-                <div className={`${styles.videoContainer}`}>
-                    <video
-                        className={`${styles.video}`}
-                        ref={videoRef}
-                        loop
-                        muted
-                        autoPlay
-                        playsInline
-                        poster={result?.fallback}
-                        preload="auto"
-                    >
-                        {/* <source src={"/home/video/videoBag.mp4"} type="video/mp4" /> */}
-                        Your browser does not support the video tag.
-                    </video>
-                </div>
-            </div>
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [result?.video_url]);
+  if (loading) return <VideoShimmer />;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textSection}>
+        {result?.title1 && (
+          <SectionHeading line1={result?.title1} line2={result?.title2} />
+        )}
+        <p className={styles.description}>{result?.description}</p>
+        <div className={styles.imageCarousel}>
+          <div className={`${styles.imagesContainer}`}>
+            {result?.image?.map((image: any, index: number) => (
+              <img key={index} src={image.image} alt="Male patient" />
+            ))}
+          </div>
+          <div className={styles.dots}>
+            <span className={styles.dot}></span>
+            <span className={styles.dot}></span>
+            <span className={styles.dot}></span>
+          </div>
         </div>
-    );
+      </div>
+      <div className={styles.videoSection}>
+        <div className={`${styles.videoContainer}`}>
+          <video
+            className={`${styles.video}`}
+            ref={videoRef}
+            loop
+            muted
+            autoPlay
+            playsInline
+            poster={result?.fallback}
+            preload="auto"
+          >
+            {/* <source src={"/home/video/videoBag.mp4"} type="video/mp4" /> */}
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ResultsSection;
--- src/components/TreatmentsDetail/index.tsx
+++ src/components/TreatmentsDetail/index.tsx
@@ -4,168 +4,185 @@ import SectionHeadPara from "../common/SectionHeadPara";
 import Faqs from "@/components/common/Faqs";
 import ResultsSection from "./ResultsSection";
 import TestimonialCarousel from "./TestimonialCarousel";
-import styles from "./Treatments.module.css"
+import styles from "./Treatments.module.css";
 import TreatmentHeroSection from "./HeroSection";
 import AppointmentForm from "../common/AppointmentForm";
 import { TreatmentDetailType } from "@/lib/types";
 import PriceTableContainer from "../common/PriceTable/PriceContainer";
 import TreatmentCardContainer2 from "./TreatmentCardContainer2";
-const TreatmentsDetail = ({ data, id }: { data: TreatmentDetailType, id?: number }) => {
-    const images = [
-        "/images/after1.png",
-        "/images/after2.png",
-        "/images/after3.png",
-    ]
-    const benefits = [
-        {
-            title: "Effective for All Skin Types",
-            description: "Works on light and dark skin tones, even tanned skin.",
-        },
-        {
-            title: "Fast Sessions",
-            description: "Covers large areas quickly, reducing treatment time.",
-        },
-        {
-            title: "More Comfortable",
-            description: "Advanced cooling technology enhances comfort during treatment.",
-        },
-        {
-            title: "Long-Lasting Results",
-            description: "Leads to desired hair reduction with multiple sessions.",
-        },
-        {
-            title: "Works on All Hair Types",
-            description: "Effective for both fine and coarse hair.",
-        },
-        {
-            title: "Fewer Sessions Needed",
-            description: "More powerful than many other lasers, requiring fewer treatments.",
-        },
-        {
-            title: "Safe and FDA-Approved",
-            description: "A proven and medically safe procedure.",
-        },
-        {
-            title: "Minimal Downtime",
-            description: "Patients can resume daily activities immediately.",
-        },
-    ];
-    const treatmentCards = [
-        {
-            title: "Laser Hair Removal",
-            description:
-                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
-            rating: 4.8,
-            customers: "300+",
-            image1: "/images/after1.png", // replace with actual image path
-            image2: "/images/after1.png", // replace with actual image path
-        },
-        {
-            title: "Laser Hair Removal",
-            description:
-                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
-            rating: 4.8,
-            customers: "300+",
-            image1: "/images/after1.png", // replace with actual image path
-            image2: "/images/after1.png", // replace with actual image path
-        },
-        {
-            title: "Laser Hair Removal",
-            description:
-                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
-            rating: 4.8,
-            customers: "300+",
-            image1: "/images/after1.png", // replace with actual image path
-            image2: "/images/after1.png", // replace with actual image path
-        },
-        {
-            title: "Laser Hair Removal",
-            description:
-                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
-            rating: 4.8,
-            customers: "300+",
-            image1: "/images/after1.png", // replace with actual image path
-            image2: "/images/after1.png", // replace with actual image path
-        },
-        {
-            title: "Laser Hair Removal",
-            description:
-                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
-            rating: 4.8,
-            customers: "300+",
-            image1: "/images/after1.png", // replace with actual image path
-            image2: "/images/after1.png", // replace with actual image path
-        },
-        {
-            title: "Laser Hair Removal",
-            description:
-                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
-            rating: 4.8,
-            customers: "300+",
-            image1: "/images/after1.png", // replace with actual image path
-            image2: "/images/after1.png", // replace with actual image path
-        },
-        {
-            title: "Laser Hair Removal",
-            description:
-                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
-            rating: 4.8,
-            customers: "300+",
-            image1: "/images/after1.png", // replace with actual image path
-            image2: "/images/after1.png", // replace with actual image path
-        },
-        {
-            title: "Laser Hair Removal",
-            description:
-                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
-            rating: 4.8,
-            customers: "300+",
-            image1: "/images/after1.png", // replace with actual image path
-            image2: "/images/after1.png", // replace with actual image path
-        },
-        {
-            title: "Laser Hair Removal",
-            description:
-                "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
-            rating: 4.8,
-            customers: "300+",
-            image1: "/images/after1.png", // replace with actual image path
-            image2: "/images/after1.png", // replace with actual image path
-        },
-
-    ]
-    return (
-        <div className={`${styles.treatmentContainer}`}>
-            <TreatmentHeroSection data={data} />
-            <AppointmentForm />
-            {data.benifits.length > 0 && data.benifits.map((benifit, index) =>
-                <>
-                    <SectionHeading key={index} line1={benifit.heading1} line2={benifit.heading2} />
-                    <SectionHeadPara key={index} para={benifit.description} />
-                    <div key={index} className={styles.patientImageContainer}>
-                        {benifit.images.map((img, idx) => <img key={idx} className={styles.patientImage} src={img.image} alt="patientImage" />)}
-                    </div>
-                </>
-            )}
-            <SectionHeading line1={`Key points about ${data.banner_text1}`} line2={`${data.banner_text2}`} />
-            <div className={styles.gridContainer}>
-                {data.keypoints.map((item, index) => (
-                    <div key={index} className={styles.card}>
-                        <strong>{item.key}</strong> – {item.value}
-                    </div>
-                ))}
+const TreatmentsDetail = ({
+  data,
+  id,
+}: {
+  data: TreatmentDetailType;
+  id?: number;
+}) => {
+  const images = [
+    "/images/after1.png",
+    "/images/after2.png",
+    "/images/after3.png",
+  ];
+  const benefits = [
+    {
+      title: "Effective for All Skin Types",
+      description: "Works on light and dark skin tones, even tanned skin.",
+    },
+    {
+      title: "Fast Sessions",
+      description: "Covers large areas quickly, reducing treatment time.",
+    },
+    {
+      title: "More Comfortable",
+      description:
+        "Advanced cooling technology enhances comfort during treatment.",
+    },
+    {
+      title: "Long-Lasting Results",
+      description: "Leads to desired hair reduction with multiple sessions.",
+    },
+    {
+      title: "Works on All Hair Types",
+      description: "Effective for both fine and coarse hair.",
+    },
+    {
+      title: "Fewer Sessions Needed",
+      description:
+        "More powerful than many other lasers, requiring fewer treatments.",
+    },
+    {
+      title: "Safe and FDA-Approved",
+      description: "A proven and medically safe procedure.",
+    },
+    {
+      title: "Minimal Downtime",
+      description: "Patients can resume daily activities immediately.",
+    },
+  ];
+  const treatmentCards = [
+    {
+      title: "Laser Hair Removal",
+      description:
+        "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+      rating: 4.8,
+      customers: "300+",
+      image1: "/images/after1.png", // replace with actual image path
+      image2: "/images/after1.png", // replace with actual image path
+    },
+    {
+      title: "Laser Hair Removal",
+      description:
+        "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+      rating: 4.8,
+      customers: "300+",
+      image1: "/images/after1.png", // replace with actual image path
+      image2: "/images/after1.png", // replace with actual image path
+    },
+    {
+      title: "Laser Hair Removal",
+      description:
+        "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+      rating: 4.8,
+      customers: "300+",
+      image1: "/images/after1.png", // replace with actual image path
+      image2: "/images/after1.png", // replace with actual image path
+    },
+    {
+      title: "Laser Hair Removal",
+      description:
+        "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+      rating: 4.8,
+      customers: "300+",
+      image1: "/images/after1.png", // replace with actual image path
+      image2: "/images/after1.png", // replace with actual image path
+    },
+    {
+      title: "Laser Hair Removal",
+      description:
+        "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+      rating: 4.8,
+      customers: "300+",
+      image1: "/images/after1.png", // replace with actual image path
+      image2: "/images/after1.png", // replace with actual image path
+    },
+    {
+      title: "Laser Hair Removal",
+      description:
+        "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+      rating: 4.8,
+      customers: "300+",
+      image1: "/images/after1.png", // replace with actual image path
+      image2: "/images/after1.png", // replace with actual image path
+    },
+    {
+      title: "Laser Hair Removal",
+      description:
+        "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+      rating: 4.8,
+      customers: "300+",
+      image1: "/images/after1.png", // replace with actual image path
+      image2: "/images/after1.png", // replace with actual image path
+    },
+    {
+      title: "Laser Hair Removal",
+      description:
+        "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+      rating: 4.8,
+      customers: "300+",
+      image1: "/images/after1.png", // replace with actual image path
+      image2: "/images/after1.png", // replace with actual image path
+    },
+    {
+      title: "Laser Hair Removal",
+      description:
+        "Look Lush Clinic treats each patient with empathy and discretion. A patient arriving at",
+      rating: 4.8,
+      customers: "300+",
+      image1: "/images/after1.png", // replace with actual image path
+      image2: "/images/after1.png", // replace with actual image path
+    },
+  ];
+  return (
+    <div className={`${styles.treatmentContainer}`}>
+      <TreatmentHeroSection data={data} />
+      <AppointmentForm />
+      {data.benifits.length > 0 &&
+        data.benifits.map((benifit, index) => (
+          <div key={index}>
+            <SectionHeading line1={benifit.heading1} line2={benifit.heading2} />
+            <SectionHeadPara para={benifit.description} />
+            <div className={styles.patientImageContainer}>
+              {benifit.images.map((img, idx) => (
+                <img
+                  key={idx}
+                  className={styles.patientImage}
+                  src={img.image}
+                  alt="patientImage"
+                />
+              ))}
             </div>
-            <TreatmentCardContainer2 id={data.treatment_id} />
+          </div>
+        ))}
+      <SectionHeading
+        line1={`Key points about ${data.banner_text1}`}
+        line2={`${data.banner_text2}`}
+      />
+      <div className={styles.gridContainer}>
+        {data.keypoints.map((item, index) => (
+          <div key={index} className={styles.card}>
+            <strong>{item.key}</strong> – {item.value}
+          </div>
+        ))}
+      </div>
+      <TreatmentCardContainer2 id={data.treatment_id} />
 
-            <TestimonialCarousel id={data.treatment_id} />
-            <ResultsSection id={data.treatment_id} />
-            <SectionHeading line1="Pricing List" />
-            <SectionHeadPara para="Looklush clinic treats each patient with empathy and discretion. A patient arriving at Looklush Clinic is educated about their condition prior to treatment." />
-            <PriceTableContainer />
-            <SectionHeading line1="Frequently Asked " line2="Questions" />
-            <Faqs />
-        </div>
-
-    )
-}
+      <TestimonialCarousel id={data.treatment_id} />
+      <ResultsSection id={data.treatment_id} />
+      {/*<SectionHeading line1="Pricing List" />
+      <SectionHeadPara para="Looklush clinic treats each patient with empathy and discretion. A patient arriving at Looklush Clinic is educated about their condition prior to treatment." />
+      <PriceTableContainer />
+      <SectionHeading line1="Frequently Asked " line2="Questions" />
+      <Faqs /> */}
+    </div>
+  );
+};
 export default TreatmentsDetail;
