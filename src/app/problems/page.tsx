0fc888929664752a524e5dcb7210f3416f4d7ffe
Aman kumar Pandey <137035478+iamanpandey11@users.noreply.github.com>
2025-05-22 14:16:36 +0530

    unused removed

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -1,13 +1,18 @@
-import Treatments from "@/components/TreatmentsDetail";
-import { Metadata } from 'next';
+import { Metadata } from "next";
 export const metadata: Metadata = {
-    title: 'Skin & Hair Problems We Treat | LookLush',
-    description: 'From acne to hair loss, LookLush addresses a variety of skin and hair concerns with effective clinical treatments.',
-    keywords: ['acne', 'hair fall', 'hyperpigmentation', 'skin concerns', 'hair loss treatment Gurugram', 'Mumbai clinic'],
+  title: "Skin & Hair Problems We Treat | LookLush",
+  description:
+    "From acne to hair loss, LookLush addresses a variety of skin and hair concerns with effective clinical treatments.",
+  keywords: [
+    "acne",
+    "hair fall",
+    "hyperpigmentation",
+    "skin concerns",
+    "hair loss treatment Gurugram",
+    "Mumbai clinic",
+  ],
 };
 
 export default function Problems() {
-    return (
-        <Treatments />
-    );
+  return <div></div>;
 }
