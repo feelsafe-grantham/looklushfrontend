7f62e170e17556d1448a1c34654ba4fc7c863126
Aman Pandey <skytechno00@gmail.com>
2025-06-13 11:21:17 +0530

    done

--- src/utils/staticNames/index.ts
+++ src/utils/staticNames/index.ts
@@ -1,6 +1,6 @@
 export const staticImages = {
   logo: "/images/looklush-logo1.png",
   logoWhite: "/images/looklush-logo-white.png",
-  topBar: "/images/topbar.png",
+  topBar: "/images/topBar.png",
   upperOfferImage: "/images/upper-offer.png",
 };
