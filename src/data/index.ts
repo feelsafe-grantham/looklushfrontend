3f5c09933bf30568ff1634a25b2171d6854c408a
Aman Pandey <skytechno00@gmail.com>
2025-06-13 17:48:16 +0530

    some changes done

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,7 +3,7 @@ export const MUMBAI_ADDRESS = {
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.4972375831276!2d72.8392009!3d19.1756447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b7dc98e75a79%3A0xea9792c966002d43!2sLooklush!5e1!3m2!1sen!2sin!4v1748327357825!5m2!1sen!2sin",
   city: "Mumbai",
   address:
-    "01, AHIMSA HEIGHT, AHIMSA MARG, NEAR SUNDAR NAGAR, OFF, Chincholi Bunder Rd, Malad West, Mumbai, Maharashtra 400064",
+    "101, AHIMSA HEIGHT, AHIMSA MARG, NEAR SUNDAR NAGAR, OFF, Chincholi Bunder Rd, Malad West, Mumbai, Maharashtra 400064",
   phone: "+91 93228 17696",
   email: "drpatelpoonam@gmail.com",
   visit_link: "https://maps.app.goo.gl/m1GjnPQs7wpRtnrt9",
@@ -14,7 +14,7 @@ export const GURUGRAM_ADDRESS = {
   city: "Gurugram",
   address:
     "R3, M3M Urbana mall, 108, Golf course extension Road, Sector 67, Gurugram, Haryana 122001",
-  phone: "+91 93228 17696",
+  phone: "+91 90826 40898",
   email: "drpatelpoonam@gmail.com",
   visit_link: "https://maps.app.goo.gl/P1k6XJHxwtPji4L39",
 };
