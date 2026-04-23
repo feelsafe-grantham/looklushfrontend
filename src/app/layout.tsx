2e7cfc34094def3872926f57ecac2ce8163e839f
Nikhil-gusain <nikstudyc03@gmail.com>
2026-04-23 12:14:52 +0530

    meta pixel updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,25 +49,23 @@ export default function RootLayout({
         {/* Meta Pixel Code */}
         <script>
           {`
-            !function(f,b,e,v,n,t,s){
-              if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)
-            }(window, document,'script',
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2478935065817522');
+            fbq('init', '1475516370741648');
             fbq('track', 'PageView');
           `}
         </script>
         <noscript>
           <img height="1" width="1" style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=2478935065817522&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1475516370741648&ev=PageView&noscript=1"
           />
         </noscript>
-
         {/* End Meta Pixel Code */}
         {/*  Google tag (gtag.js)  */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17058145078"></script>
